74a130127205068bb24062ec197149fe84e5853b
Clement Wee <clement.wee.personal@gmail.com>
2021-06-30 23:32:14 +0800

    Add Flatten implementation

---
+++ src/hx/monaxe/reactive/observable/Flatten.hx
@@ -0,0 +1,44 @@
+package monaxe.reactive.observable;
+
+import monaxe.execution.MultiAssignCancellable;
+import monaxe.execution.Cancellable;
+import haxe.ds.List;
+
+class Flatten<T> {
+
+    public var gatheredObservables: List<Observable<T>>;
+    public var isComplete: Bool;
+
+    static public function flatten<T>(upS: Observable<Observable<T>>): Observable<T>{
+
+        return (obs: Observer<T>) -> {
+            var instance = new Flatten();
+            var combined = new MultiAssignCancellable();
+            combined.add(upS.subscribe(instance.subscriber(obs)));
+            combined.add(instance.subscribe(obs));
+            return combined;
+        }
+    }
+
+    public function new(){
+        this.gatheredObservables = new List();
+        this.isComplete = false;
+    }
+
+    function doNothing(){}
+
+    function subscribe(downS: Observer<T>): Cancellable{
+        var timer = new FlattenTimer(downS, this);
+        return timer.getCancellable();
+    }
+
+    public function subscriber(downS: Observer<T>): Observer<Observable<T>>{
+        return evt -> 
+        switch evt {
+            case Start: doNothing();
+            case Event(o): this.gatheredObservables.add(o);
+            case Complete: isComplete = true;
+            case Error(msg): downS.onError(msg); // pipe errors directly to downStream
+        }
+    }
+}
---
+++ src/hx/monaxe/reactive/observable/FlattenTimer.hx
@@ -0,0 +1,47 @@
+package monaxe.reactive.observable;
+
+import haxe.ds.List;
+import monaxe.reactive.observer.EventOrState;
+import monaxe.execution.SerialAssignCancellable;
+import monaxe.execution.Cancellable;
+
+class FlattenTimer<T> extends haxe.Timer {
+    var cancellable: SerialAssignCancellable;
+    var advanceObs: Observer<T>;
+    var isChildComplete: Bool;
+    var nextChild: Null<Observable<T>>;
+    var downS: Observer<T>;
+    var parent: Flatten<T>;
+
+    public function new(downS: Observer<T>, parent: Flatten<T>){
+        super(100);
+        this.cancellable = new SerialAssignCancellable();
+        isChildComplete = true;
+        this.advanceObs = (evt: EventOrState<T>) -> switch evt {
+            case Start: isChildComplete = false;
+            case Event(e): downS.onData(e);
+            case Error(msg): downS.onError(msg);
+            case Complete: isChildComplete = true;
+        }
+        this.nextChild = null;
+        this.parent = parent;
+    }
+
+    override public function run(){ // a polling function
+        if(isChildComplete){
+            nextChild = this.parent.gatheredObservables.pop();
+            if(nextChild!=null)  cancellable.assign(nextChild.subscribe(advanceObs));
+            else {
+                if(this.parent.isComplete) {
+                    stop();
+                    downS.onComplete();
+                }
+                //else wait for next tick
+            }
+        }
+    }
+
+    public function getCancellable(){
+        return cancellable;
+    }
+}
